5338ca6fc35797990c97157a1a06c00bad987081
dservo <arrasco.cs@gmail.com>
2020-05-13 19:10:17 -0500

    Boostrap on Template

--- posts/views.py
+++ posts/views.py
@@ -1,40 +1,42 @@
 """Posts views."""
 
 # Django
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Utilities
 from datetime import datetime
 
 posts = [
-	{
-		'name': 'Escribiendo Pensamientos',
-		'user': 'kpaiva',
-		'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-		'picture': 'https://i.picsum.photos/id/4/200/300.jpg?grayscale',
-	},
-	{
-		'name': 'Perrito cochino',
-		'user': 'dservo',
-		'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-		'picture': 'https://i.picsum.photos/id/237/200/300.jpg',
-	},
-	{
-		'name': 'Nieve en las rocas',
-		'user': 'jperezoso',
-		'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-		'picture': 'https://i.picsum.photos/id/866/200/300.jpg',
-	},	
+    {
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/600?image=1036',
+    },
+    {
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/800/?image=903',
+    },
+    {
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/500/700/?image=1076',
+    }
 ]
 
-def list_posts(requests):
+def list_posts(request):
 	"""List existing posts."""
-	content = []
-	for post in posts:
-		content.append("""
-			<p><strong>{name}</strong></p>
-			<p><small>{user} - <i>{timestamp}</i></small></p>
-			<figure><img src="{picture}"/></figure>
-		""".format(**post))
-
-	return HttpResponse('<br>'.join(content))
+	#return render(request, 'feed.html', {'name': 'Dernier Servo'}) #3rd Parameter: Context, that is a dictionary
+	return render(request, 'feed.html', {'posts': posts})
