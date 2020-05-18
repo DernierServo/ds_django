1ce372e0f5e5c6effe54f770938b9c4366077f61
dservo <arrasco.cs@gmail.com>
2020-05-18 10:14:17 -0500

    Admin model created

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,18 +1,18 @@
 #ESTE ARCHIVO RELACIONA URLS CON SUS VISTAS CORRESPONDIENTES
 #ds: Una vista en Django es una función!
 
-#from django.contrib import admin
+# Django
+from django.contrib import admin
 from django.urls import path
+
 from platzigram import views as local_views
 from posts import views as posts_views
 
 
-
 #ds: Todas las vistas reciben un "request"
 
-
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('hello-world', local_views.hello_world),
     path('sorted/', local_views.sort_integers),
     path('hi/<str:name>/<int:age>/', local_views.say_hi),
